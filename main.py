980019ab18e234cfbae3ba0ea76c66914f126167
dnavaria <dk17816@gmail.com>
2023-07-08 12:31:18 +0000

    logging implemented

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import os
 import asyncio
-import aiohttp, aiofiles
+import aiohttp
+import logging
+import aiofiles
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
 
-async def download_assets(url, directory):
+async def download_assets(url, base_directory="assets"):
     # Create a directory based on the URL
     directory_name = urlparse(url).netloc
+    directory_name = os.path.join(base_directory, directory_name)
     os.makedirs(directory_name, exist_ok=True)
+
+    # Set up logging
+    log_filename = os.path.join(directory_name, "download.log")
+    logging.basicConfig(
+        filename=log_filename,
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s"
+    )
 
     # Send a GET request to the URL
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status != 200:
-                print(f"Failed to retrieve {url}")
+                logging.error(f"Failed to retrieve {url}")
                 return
 
             # Parse the HTML content
@@ -44,9 +55,9 @@ async def download_image(url, directory, session):
             filename = os.path.join(directory, os.path.basename(url))
             async with aiofiles.open(filename, "wb") as f:
                 await f.write(await response.content.read())
-            print(f"Image saved: {filename}")
+            logging.info(f"Image saved: {filename}")
         else:
-            print(f"Failed to download image: {url}")
+            logging.error(f"Failed to download image: {url}")
 
 
 async def download_asset(url, directory, session):
@@ -55,12 +66,12 @@ async def download_asset(url, directory, session):
             filename = os.path.join(directory, os.path.basename(url))
             async with aiofiles.open(filename, "wb") as f:
                 await f.write(await response.content.read())
-            print(f"Asset saved: {filename}")
+            logging.info(f"Asset saved: {filename}")
         else:
-            print(f"Failed to download asset: {url}")
+            logging.error(f"Failed to download asset: {url}")
 
 
 # change the website_url to download assets from a different website
 website_url = "https://websitedemos.net/organic-shop-02/"
 
-asyncio.run(download_assets(website_url, directory=website_url))
+asyncio.run(download_assets(website_url))
